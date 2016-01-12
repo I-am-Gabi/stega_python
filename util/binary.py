efe7b208cda54d5d04345e1c2c1ba1c94383197f
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-01-12 22:59:52 +0100

    add util -- binary manipulation

---
+++ util/binary.py
@@ -0,0 +1,11 @@
+
+
+def get_bit(int_type, offset):
+    return (int_type & (1 << offset)) >> offset
+
+
+def set_bit(int_type, offset, value):
+    if value:
+        return int_type | (1 << offset)
+    else:
+        return int_type & ~(1 << offset)

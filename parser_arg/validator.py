baeba14da147a1293243c2debe2453ff09b3fd7a
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-01-12 17:23:04 +0100

    add try to exception

--- parser_arg/validator.py
+++ parser_arg/validator.py
@@ -1,9 +1,8 @@
 import os.path
-from image_processing.exceptions.FileInputError import FileInputError
 
 
 def validator_file(file):
     try:
         return os.path.isfile(file)
-    except FileInputError as e:
-        "I/O error({0}): {1}".format(e.errno, e.strerror)
+    except TypeError as e:
+        "I/O error({0})".format(e)

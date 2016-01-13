3d87a10a009a444a9a49578a9c981baf596e9b81
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-01-13 12:04:54 +0100

    fix the file write

--- reveal.py
+++ reveal.py
@@ -1,25 +1,16 @@
 from parser_arg.parser import parser
 from stegano.stegano import reveal
+import filecmp
 
 
 def main():
     arg = parser()
 
     msg = reveal(arg)
-    string = ""
+    file_output = open(arg[1], 'wb')
+    file_output.write(str(msg))
 
-    file = open(arg[1],'ab')
-
-    for c in msg:
-        if c != 255:
-            string += chr(c)
-    file.write(str(string))
-    #print msg[0]
-    #print chr(msg[0])
-    #file.write(str(unichr(c) for c in msg))
-    #file.write((''.join(chr(i) for i in msg)).encode('ascii'))
-    #fileByteArray = bytearray(msg)
-    #file.write(str(msg))
+    print filecmp.cmp("resources/txt/originaltext1.txt", arg[1], shallow=False)
 
 if __name__ == "__main__":
     main()

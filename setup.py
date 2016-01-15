326a066291ecac9db050aa97d526436c4a2cdd9c
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-01-15 19:57:14 +0100

    setup to the travis #7

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Steganography',
+      version='1.0',
+      description='Python Steganography Tools',
+      author='Gabriela Cavalcante',
+      author_email='gabicavalcantesilva@gmail.com',
+      url='/',
+      packages=['image', 'parser_arg', 'resources', 'stegano', 'stegano.patterns', 'tests'],
+      )
